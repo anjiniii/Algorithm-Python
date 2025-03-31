d2a42cb68ed146e414c2509c6a101244b7127a7c
Anjin Lee <anjin720jin@naver.com>
2025-04-01 00:53:12 +0900

    LeetCode/347 추가 풀이

--- 3-leetcode/347-top-k-frequent-elements.py
+++ 3-leetcode/347-top-k-frequent-elements.py
@@ -11,4 +11,11 @@ def solution(nums, k):
     print(answer)
 
 
-solution(nums=[1, 1, 1, 2, 2, 3], k=2)
+def solution2(nums, k):
+    counter = Counter(nums)
+    most_commons = counter.most_common(k)
+    answer = [i[0] for i in most_commons]
+    print(answer)
+
+
+solution2(nums=[1, 1, 1, 2, 2, 3], k=2)
